a57a70a9c2c93ad35c6d50e6bf272fa27814d2fe
Ayddan <nathanleduc@hotmail.fr>
2023-03-20 16:08:44 +0100

    Desktop header

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Header from './components/Header'
+
+import './styles/css/style.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+    </div>
+  )
+}
+
+export default App
