3880bfbabfeedb39ed644fd542a5a24967591021
Ayddan <nathanleduc@hotmail.fr>
2023-03-21 11:05:40 +0100

    Add mobile support for header

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,12 @@ import Header from './components/Header'
 import './styles/css/style.css'
 
 function App() {
+  const [ menuIsOpen, setMenuIsOpen ] = useState(false);
+
   return (
     <div className="App">
-      <Header/>
+      <div className={`background-mask ${menuIsOpen ? 'active' : null}`} onClick={()=>setMenuIsOpen(false)}></div>
+      <Header handleMenuToggle={(isOpen)=>setMenuIsOpen(isOpen)} menuIsOpen={menuIsOpen}/>
     </div>
   )
 }
